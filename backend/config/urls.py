5f25d6659d7324a54be6e1ab7902d8ecdd156fb3
Jakub Kogut <jakub@jkogut.pl>
2025-12-07 23:41:23 +0100

    docs swagger fix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
